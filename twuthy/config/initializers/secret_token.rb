a0b3a179559857fa967529498d41af219c624ae2
Alicia <amstott2@gmail.com>
2012-07-20 22:40:55 -0400

    First commit

---
+++ twuthy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twuthy::Application.config.secret_token = '9a7b478d51168598203fca2ec059673c2b32281d2d763c10bb36c2246f29ded3d92c591a52a6fee295d548dcfe641c78102e2d56d267f2a93b62236d42c20e04'
