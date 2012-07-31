c71b565a63b8685ee1ac9002a5f78acf7ddf07aa
Alicia <amstott2@gmail.com>
2012-07-30 22:05:11 -0400

    Added route for voting functionality

--- twuthy/config/routes.rb
+++ twuthy/config/routes.rb
@@ -1,5 +1,5 @@
 Twuthy::Application.routes.draw do
-  resources :voteable_models
+
 
   resources :tweets
   # The priority is based upon order of creation:
@@ -57,5 +57,5 @@ Twuthy::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
+  match ':controller(/:action(/:id(.:format)))'
 end
